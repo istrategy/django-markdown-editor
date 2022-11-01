4078acc77bbc09148771821334dade99e8446ab5
istrategy <info@itinnovate.co.za>
2022-11-01 08:00:35 +0200

    Add build docker & start project

---
+++ r.sh
@@ -0,0 +1,3 @@
+docker build -t django-markdown-editor .
+
+docker run -it -p 8020:8020 --name DJANGOTEST -e DJANGO_SUPERUSER_USERNAME=admin -e DJANGO_SUPERUSER_PASSWORD=sekret1 -e DJANGO_SUPERUSER_EMAIL=admin@example.com django-markdown-editor
